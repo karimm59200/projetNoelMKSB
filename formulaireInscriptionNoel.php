03185a956364de7fac3a0c2963aed86b486dfb6d
SamG10 <samuel100800@gmail.com>
2021-12-08 12:07:23 +0100

    4

---
+++ formulaireInscriptionNoel.php
@@ -0,0 +1,34 @@
+<?php
+require_once "ressources.php";
+
+if ($_POST) {
+    $pseudo = $_POST['pseudo'];
+
+    $query  = "insert into inscription (pseudo) values ( '$pseudo' );";
+    $res = query($query);
+    if ($res)
+        print("<h3> $pseudo enregistré</h3>\n");
+    else
+        print("<h3>erreur enregistrement !!!</h3>\n");
+}
+?>
+<form action="#" method="POST">
+    <input type="text" name='pseudo' placeholder=" entre ton pseudo" required>
+    <br>
+
+    <input type="checkbox" name="cadeau" OnClick="checkboxx();" id="checkbox" />
+    <label>Je confirme avoir un cadeau</label><br>
+    <button type="submit" name="envoyer" id="submit" disabled="disabled" >Ajouter</button>
+    
+    <br>
+    
+</form>
+<script type="text/javascript">
+    function checkboxx() {
+        if (document.getElementById('checkbox').checked ) {
+            document.getElementById('submit').disabled = false;
+        } else {
+            document.getElementById('submit').disabled = true;
+        }
+    }
+</script>
