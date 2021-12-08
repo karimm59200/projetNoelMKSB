1d598b8512a9dd3160955550199c9d13d8418b18
SamG10 <samuel100800@gmail.com>
2021-12-08 09:41:45 +0100

    08-12-2021

--- admin.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE IF EXISTS admin;
-
-CREATE TABLE admin (id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, nom VARCHAR(30), pw VARCHAR(50));
-
-` inséré l'administrateur via la page inscriptionAdmin(TEMPORAIRE) !!!`
